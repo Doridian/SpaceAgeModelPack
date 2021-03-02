4ad1f7dd5126f3e67697f78c787f612db6e1139f
Mark Dietzer <git@doridian.net>
2021-03-01 19:45:15 -0800

    Merge PR from Buggy. More accurate tib physics

--- publish.bat
+++ publish.bat
@@ -1,2 +1,3 @@
-..\..\..\bin\gmad create -folder .\ -out ..\SpaceAgeModelPack.gma
-..\..\..\bin\gmpublish update -id 1869364824 -addon ..\SpaceAgeModelPack.gma -icon icon.jpg
+"C:\Program Files (x86)\Steam\steamapps\common\GarrysMod\bin\gmad" create -folder .\ -out ..\SpaceAgeModelPack.gma
+"C:\Program Files (x86)\Steam\steamapps\common\GarrysMod\bin\gmpublish" update -id 1869364824 -addon ..\SpaceAgeModelPack.gma -icon icon.jpg
+del  ..\SpaceAgeModelPack.gma
